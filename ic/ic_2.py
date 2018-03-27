eb46cffc477e76473c8d6e5977bf6ca782936b1d
Peter Tountas <peter.tountas@shiftgig.com>
2018-03-27 09:42:02 -0500

    reps

---
+++ ic/ic_2.py
@@ -0,0 +1,33 @@
+
+# You have a list of integers, and for each index you want to
+# find the product of every integer except the integer at that index.
+
+#greedy approach
+
+
+nums = [3, 2, 5] 
+
+def get_products_of_ints_except_at_index(int_list):
+
+    if len(int_list) < 2:
+        raise ValueError('Need more integers in list')
+
+    products_of_all_ints_except_at_index = len(int_list) * [None]
+
+    product_so_far = 1
+
+    for i in xrange(len(int_list)):
+        products_of_all_ints_except_at_index[i] = product_so_far
+        product_so_far *= int_list[i]
+
+
+    product_so_far = 1
+    for i in xrange(len(int_list) - 1, -1, -1):
+        products_of_all_ints_except_at_index[i] *= product_so_far
+        product_so_far *= int_list[i]
+
+    print products_of_all_ints_except_at_index
+
+
+
+get_products_of_ints_except_at_index(nums)
