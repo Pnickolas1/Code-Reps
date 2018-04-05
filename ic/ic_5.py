3c643b21a1a368a06cbd604c51a54f5c4a4a1b4a
Peter Tountas <peter.tountas@shiftgig.com>
2018-04-05 09:12:12 -0500

    dynamic programming   bottom up approach

--- ic/ic_5.py
+++ ic/ic_5.py
@@ -5,10 +5,29 @@
 # computes the number of ways to make the amount of money with coins of the available
 
 
-class Change(object):
+def change_possibilities_bottom_up(amount, denominations):
 
-    def __init__(self):
-        self.memo = {}
+    ways_of_doing_n_cents = [0] * (amount + 1)
+    ways_of_doing_n_cents[0] = 1
 
-        def change_possibilities_top_down(self, amount_left, denominations, current_index= 0):
-            
+    for coin in denominations:
+
+        for higher_amount in xrange(coin, amount + 1):
+            higher_amount_remainder = higher_amount - coin
+            ways_of_doing_n_cents[higher_amount] += ways_of_doing_n_cents[higher_amount_remainder]
+
+
+    print '# of Possibilities: ', ways_of_doing_n_cents[amount]
+
+coins = [1,3,5]
+amount = 5
+
+change_possibilities_bottom_up(amount, coins)
+
+
+
+# take aways: Dynamic Programming
+# bottom up algorithm: avoid recursive approach and ultimately the potential to build up a large call stack that
+# could cause a `stack overflow error`
+#
+
