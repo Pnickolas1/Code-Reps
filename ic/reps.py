24cf354069d55739c8f39b70151042d07edd9d9e
Peter Tountas <peter.tountas@shiftgig.com>
2018-05-04 16:39:01 -0500

    adding a dir for algorithm self study

--- ic/reps.py
+++ ic/reps.py
@@ -5,32 +5,32 @@ def highest_product_of_three(inputs):
 
     # edge case
     if len(inputs) < 3:
-        raise ValueError('we need more inputs')
+        raise ValueError('need more than 2 inputs')
 
     # storage and setup
-    highest = max(inputs[0], inputs[1])
-    lowest = min(inputs[0], inputs[1])
     highest_product_of_3 = inputs[0] * inputs[1] * inputs[2]
     highest_product_of_2 = inputs[0] * inputs[1]
     lowest_product_of_2 = inputs[0] * inputs[1]
+    highest = max(inputs[0], inputs[1])
+    lowest = min(inputs[0], inputs[1])
 
-    # iterate through
+    # iterate throught he problem
     for i in xrange(2, len(inputs)):
 
         current = inputs[i]
+
         highest_product_of_3 = max(highest_product_of_3,
                                    current * highest_product_of_2,
-                                   current * lowest_product_of_2,
+                                   current * lowest_product_of_2
                                    )
 
         highest_product_of_2 = max(highest_product_of_2,
                                    current * highest,
                                    current * lowest
                                    )
-
         lowest_product_of_2 = min(lowest_product_of_2,
                                   current * highest,
-                                  current * lowest
+                                  current * lowest,
                                   )
 
         highest = max(current, highest)
@@ -38,6 +38,7 @@ def highest_product_of_three(inputs):
         lowest = min(current, lowest)
 
     print highest_product_of_3
+
 
 inps = [-1, 5, 10, 4]
 
