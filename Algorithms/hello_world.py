590eee36f64fe3e72f28caa4862e8c25acd48b5e
Peter <petertountas1@gmail.com>
2019-01-14 10:34:51 -0600

    updating file structure for data structures

---
+++ Algorithms/hello_world.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+def hello_world(my_name):
+
+    print 'hello world my name is ', my_name
+
+
+hello_world('lauren')
+
+
+
+def hello_world_sibling(my_name, sister):
+
+    print "hello world my name is", my_name, "and I have a sister named", sister
+
+
+hello_world_sibling('lauren','andriana')
+
--- Algorithms/quickSort.py
+++ Algorithms/quickSort.py
@@ -2,6 +2,7 @@
 def quick_sort(arr):
 
     quick_sort_help(arr ,0 ,len(arr ) -1)
+    print(arr)
 
 def quick_sort_help(arr ,first ,last):
 
@@ -39,47 +40,7 @@ def partition(arr, first, last):
     arr[rightmark] = temp
 
     return rightmark
-def quick_sort(arr):
 
-    quick_sort_help(arr,0,len(arr)-1)
+random_numbers = [24,40,23,98,44,19,8,31,31,8,38,0,35,50,3,45,44,41,5,16,29,25,25,32,25,20,34,36,3,19]
 
-def quick_sort_help(arr,first,last):
-
-    if first<last:
-
-
-        splitpoint = partition(arr,first,last)
-
-        quick_sort_help(arr,first,splitpoint-1)
-        quick_sort_help(arr,splitpoint+1,last)
-
-
-def partition(arr,first,last):
-
-    pivotvalue = arr[first]
-
-    leftmark = first+1
-    rightmark = last
-
-    done = False
-    while not done:
-
-        while leftmark <= rightmark and arr[leftmark] <= pivotvalue:
-            leftmark = leftmark + 1
-
-        while arr[rightmark] >= pivotvalue and rightmark >= leftmark:
-            rightmark = rightmark -1
-
-        if rightmark < leftmark:
-            done = True
-        else:
-            temp = arr[leftmark]
-            arr[leftmark] = arr[rightmark]
-            arr[rightmark] = temp
-
-    temp = arr[first]
-    arr[first] = arr[rightmark]
-    arr[rightmark] = temp
-
-
-    return rightmark
+quick_sort(random_numbers)
