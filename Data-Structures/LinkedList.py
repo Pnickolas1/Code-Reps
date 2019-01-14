88b972c663df8bdbd746d6db2a5259c3689bc4a7
Peter <petertountas1@gmail.com>
2019-01-14 10:53:22 -0600

    updating the linkedlist code

--- Data-Structures/LinkedList.py
+++ Data-Structures/LinkedList.py
@@ -82,13 +82,13 @@ class LinkedList(object):
         current = self.head
         previous = None
         while current.value != value and current.next:
-          previous = current
-          current = current.next
+            previous = current
+            current = current.next
         if current.value == value:
-          if previous:
-              previous.next = current.next
-          else:
-              self.head = current.next
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
 
 # Test cases
 # Set up some Elements
@@ -97,8 +97,32 @@ e2 = Element(2)
 e3 = Element(3)
 e4 = Element(4)
 
+print (e1, e2, e3, e4)
 
-print (e1, e2)
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+
+# Test get_position
+# Should print 3q
+print ll.head.next.next.value
+# Should also print 3
+print ll.get_position(3).value
+
+# Test insert
+ll.insert(e4,3)
+# Should print 4 now
+print ll.get_position(3).value
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print ll.get_position(1).value
+# Should print 4 now
+print ll.get_position(2).value
+# Should print 3 now
+print ll.get_position(3).value
 
 
 
