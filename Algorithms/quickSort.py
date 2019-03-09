4714f40c408b340d043d4a616df021b2796f6d70
Peter <petertountas1@gmail.com>
2019-03-09 13:18:02 -0600

    updating

--- Algorithms/quickSort.py
+++ Algorithms/quickSort.py
@@ -1,52 +1,9 @@
-
-def quick_sort(arr):
-
-    quick_sort_help(arr ,0 ,len(arr ) -1)
-    print(arr)
-
-def quick_sort_help(arr ,first ,last):
-
-    if first< last:
-        splitpoint = partition(arr, first, last)
-
-        quick_sort_help(arr, first, splitpoint - 1)
-        quick_sort_help(arr, splitpoint + 1, last)
-
-
-def partition(arr, first, last):
-    pivotvalue = arr[first]
-
-    leftmark = first + 1
-    rightmark = last
-
-    done = False
-    while not done:
-
-        while leftmark <= rightmark and arr[leftmark] <= pivotvalue:
-            leftmark = leftmark + 1
-
-        while arr[rightmark] >= pivotvalue and rightmark >= leftmark:
-            rightmark = rightmark - 1
-
-        if rightmark < leftmark:
-            done = True
-        else:
-            temp = arr[leftmark]
-            arr[leftmark] = arr[rightmark]
-            arr[rightmark] = temp
-
-    temp = arr[first]
-    arr[first] = arr[rightmark]
-    arr[rightmark] = temp
-
-    return rightmark
-
-random_numbers = [24,40,23,98,44,19,8,31,31,8,38,0,35,50,3,45,44,41,5,16,29,25,25,32,25,20,34,36,3,19]
-
-quick_sort(random_numbers)
-
 from random import randint
+random_numbers = [24, 40, 23, 98, 44, 19, 8, 31, 31, 8, 38, 0, 35, 50, 3,
+                  45, 44, 41, 5, 16, 29, 25, 25, 32, 25, 20, 34, 36, 3, 19]
 
+def quick_sort(a):
+    inPlaceQuickSort(a, 0,  len(a) -1)
 
 def inPlaceQuickSort(A, start, end):
     if start < end:
@@ -66,7 +23,7 @@ def inPlacePartition(A, start, end):
     A[end] = A[pivot]
     A[pivot] = temp
     newPivotIndex = start - 1
-    for index in xrange(start, end):
+    for index in range(start, end):
         if A[index] < A[end]:  # check if current val is less than pivot value
             newPivotIndex = newPivotIndex + 1
             temp = A[newPivotIndex]
@@ -80,4 +37,50 @@ def inPlacePartition(A, start, end):
 
 X = [4, 5, 7, 4, 3, 6, 0, 4, 22, 45, 82]
 inPlaceQuickSort(X, 0, len(X) - 1)
-# X=[0, 3, 4, 4, 4, 5, 6, 7, 22, 45, 82]
+
+print('pre sort', random_numbers)
+quick_sort(random_numbers)
+print(random_numbers)
+
+
+
+
+# this is another way
+
+
+def quicksort(alist, start, end):
+    '''This function calls the partition and then recurse itself using the index returned by partition'''
+    if start < end:
+        pIndex = partition(alist, start, end)
+        quicksort(alist, start, pIndex - 1)
+        quicksort(alist, pIndex + 1, end)
+
+    return alist
+
+
+def partition(alist, start, end):
+    """
+    This function will devide the list in two halves wrt pivot
+    This will return the index of pivot after deviding
+    """
+    pivot = randint(start, end)
+    temp = alist[end]
+    alist[end] = alist[pivot]
+    alist[pivot] = temp
+    pIndex = start
+
+    for i in range(start, end):
+        if alist[i] <= alist[end]:
+            temp = alist[i]
+            alist[i] = alist[pIndex]
+            alist[pIndex] = temp
+            pIndex += 1
+    temp1 = alist[end]
+    alist[end] = alist[pIndex]
+    alist[pIndex] = temp1
+
+    return pIndex
+
+if __name__ == '__main__':
+    list = [2, 5, 6, 8, 3, 7, 5, 6, 20, 100, 85, -20]
+    print(quicksort(list, 0, len(list) - 1))
--- scratch_file.py
+++ scratch_file.py
@@ -1,30 +1,47 @@
-import sys
+from random import randint
 random_numbers = [24, 40, 23, 98, 44, 19, 8, 31, 31, 8, 38, 0, 35, 50, 3,
-                  45, 44, 41, 5, 16, 29, 25, 25, 32, 25, 20, 34, 36, 3, 19]
-
+                   45, 44, 41, 5, 16, 29, 25, 25, 32, 25, 20, 34, 36, 3, 19]
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 
-def selection_sort(arr):
+def quicksort(arr, start, end):
 
-    for fillslots in range(len(arr)- 1, 0, -1):
-
-        position_of_max = 0
-
-        for location in range(1, fillslots + 1):
-
-            if arr[location] > arr[position_of_max]:
-                position_of_max = location
-
-        temp = arr[fillslots]
-        arr[fillslots] = arr[position_of_max]
-        arr[position_of_max] = temp
-
+    if start < end:
+        pIndex = partition(arr, start, end)
+        quicksort(arr, start, pIndex -1)
+        quicksort(arr, pIndex + 1, end)
     return arr
 
 
+def partition(arr, start, end):
 
-print(selection_sort(random_numbers))
+    # gettting a random integer from the index range of start and end
+    pivot = randint(start, end)
+
+    # setting temp as the last item in the array
+    temp = arr[end]
+
+    # overwriting the last item in the array w/ the pivot index's value
+    arr[end] = arr[pivot]
+
+    # overwrite the location of the pivot index with the last item in the array
+    arr[pivot] = temp
+
+    # starting point, the first item in the array
+    pIndex = start
+
+    for i in range(start, end):
+        if arr[i] <= arr[end]:
+            temp = arr[i]
+            arr[i] = arr[pIndex]
+            arr[pIndex] = temp
+            pIndex += 1
+    temp1 = arr[end]
+    arr[end] = arr[pIndex]
+    arr[pIndex] = temp1
+
+    return pIndex
 
 
-
+print(quicksort(random_numbers, 0, len(random_numbers) -1))
+2
