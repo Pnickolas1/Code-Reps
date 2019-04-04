8187e6708619def3291249e58b46ba3c04e4f13e
Peter Tountas <petertountas1@gmail.com>
2019-04-04 06:54:41 -0500

    updating the folder

---
+++ Bitwise_Operators/And.py
@@ -0,0 +1,19 @@
+
+
+
+
+def count_bits(x):
+
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
+
+
+
+
+
+if __name__ == "__main__":
+    print(count_bits(10))
+    print('binary', bin(10))
