13f5ed7ecd054529291490f83b869c82fc3ccb3e
Peter Tountas <petertountas1@gmail.com>
2019-04-07 21:07:00 -0500

    updating the docstring

--- Arrays/dutch_national_flag.py
+++ Arrays/dutch_national_flag.py
@@ -1,6 +1,12 @@
 
 import os
 
+"""
+write a program that takes an array A and an index i into A and rearranges the elements such that
+all elements less than A[i] (the "pivot") appear first, followed by, elements equal to the pivot
+followed by elements greater than the pivot
+"""
+
 RED, WHITE, BLUE = range(3)
 
 def dutch_flag_partition(pivot_index, arr):
