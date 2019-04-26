88c117c11fa7b192799a02a9b4ecc7d2dadd0e05
Peter <petertountas1@gmail.com>
2019-04-26 08:55:00 -0500

    linked list constructor

--- AlgoExpert/Constructions/LinkedList_construction.py
+++ AlgoExpert/Constructions/LinkedList_construction.py
@@ -37,6 +37,36 @@ space: O(1)
 
 """
 
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        pass
+
+    def setTail(self, node):
+        pass
+
+    def insertBefore(self, node, nodeToInsert):
+        pass
+
+    def insertAfter(self, node, nodeToInsert):
+        pass
+
+    def insertAtPosition(self, position, node):
+        pass
+
+    def removeNodesWithValue(self, value):
+        pass
+
+    def remove(self, node):
+        pass
+
+    def containsNodeWithValue(self, value):
+        pass
+
 
 
 
