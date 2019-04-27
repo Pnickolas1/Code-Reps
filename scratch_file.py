de6e42a2540c597266f58a3263090e5cd53e05fe
Peter Tountas <petertountas1@gmail.com>
2019-04-27 11:58:12 -0500

    adding depth firt search

--- scratch_file.py
+++ scratch_file.py
@@ -5,23 +5,16 @@ random_numbers = [24, 40, 23, 98, 44, 19, 8, 31, 31, 8, 38, 0, 35, 50, 3,
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 """
+
+space: O(n)
+time: O(n)
+
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393,
 196418, 317811,
 """
 
 def twoNumberSum(arr, targetNumber):
-
-    nums = {}
-
-    for num in arr:
-        target = targetNumber - num
-        if target in nums:
-            return sorted([num, target])
-        else:
-            nums[num] = True
-
-    return []
 
 
 print(twoNumberSum([3, 4,8, 12, 13, 30, 2], 34))
