346f6b662c41f98fd13c77e1313f703229877b84
Peter Tountas <petertountas1@gmail.com>
2019-05-07 08:26:23 -0500

    bst construction

---
+++ AlgoExpert/Constructions/binary_search_tree_construction.py
@@ -0,0 +1,40 @@
+
+
+""""
+
+
+All BSTS should be have ability to
+
+Average Case:
+    Time: O(log n)      (for all three operations below), n is the number of nodes in the BST
+    Space: O(log n)     (if implemented recursively)
+
+Worst Case:
+    Time: O(n)
+    Space: O(n)
+
+ - Insert
+
+
+ - Search'
+ - Delete
+
+
+When Deleting **
+you replace the node to be deleted, you replace it the SMALLEST VALUE IN THE RIGHT SUBTREE
+of that node, grab the left most value in the right subtree
+
+
+
+
+
+"""
+
+
+class Binary_search_tree:
+
+    def __init__(self):
+
+
+
+
