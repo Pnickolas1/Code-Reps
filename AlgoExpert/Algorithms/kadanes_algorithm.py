e6ff50cedbc1377959911f4fdb79570f2edeac6a
Peter Tountas <petertountas1@gmail.com>
2019-05-17 08:40:49 -0500

    updating the kadanes algorithm

--- AlgoExpert/Algorithms/kadanes_algorithm.py
+++ AlgoExpert/Algorithms/kadanes_algorithm.py
@@ -19,8 +19,8 @@ Time: O(n)
 def kadanesAlgorithm(array):
     maxEndingHere = array[0]
     maxSoFar = array[0]
-    for num in array[1:]:
-        maxEndingHere = max(num, maxEndingHere + num)
+    for i in range(1, len(array)):
+        maxEndingHere = max(array[i], maxEndingHere + array[i])
         maxSoFar = max(maxSoFar, maxEndingHere)
     return maxSoFar
 
