e321e7ccd86d2c65fe22b3aee89e2abb6f1f06d0
Peter <petertountas1@gmail.com>
2019-05-29 15:49:07 -0500

    updatingthe

--- AlgoExpert/Array/three_number_sum.py
+++ AlgoExpert/Array/three_number_sum.py
@@ -34,6 +34,20 @@ def three_number_sum(array, targetSum):
     return triplets
 
 
+class Node:
 
+    def __init__(self, value):
+        self.value = value
+        self.children = []
 
+    def addChild(self, value):
+        self.children.append(Node(value))
 
+    def breadthFirstSearch(self, array):
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            array.append(current.value)
+            for child in current.children:
+                queue.append(child)
+        return array
