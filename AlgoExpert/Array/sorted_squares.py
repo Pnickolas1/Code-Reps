7cfe4060f731f88322e2291eb139b8cbf63b5ede
Peter <petertountas1@gmail.com>
2019-06-05 17:01:09 -0500

    updating sorted sqaures questions

---
+++ AlgoExpert/Array/sorted_squares.py
@@ -0,0 +1,41 @@
+
+nums = [-10, -8, -3, -2, -1, 4, 5, 6, 50, 200]
+
+
+def sortedSquares(array):
+
+    negs = []
+    pos = []
+    ans = []
+
+    for num in array:
+        if num < 0:
+            negs.append(num**2)
+        else:
+            pos.append(num**2)
+    print(negs, pos)
+    np = len(negs) - 1
+    pp = 0
+
+    while np >= 0 and pp < len(pos):
+        if negs[np] < pos[pp]:
+            ans.append(negs[np])
+            np -= 1
+        else:
+            ans.append(pos[pp])
+            pp += 1
+
+    while pp < len(pos):
+        ans.append(pos[pp])
+        pp += 1
+
+    while np >= 0:
+        ans.append(negs[np])
+        np -= 1
+
+
+    return ans
+
+
+
+print(sortedSquares(nums))
