780e523110377972ba249935b37571af48dbb627
Peter <petertountas1@gmail.com>
2019-06-18 10:50:25 -0500

    updating the min rewards

--- scratch_file.py
+++ scratch_file.py
@@ -39,22 +39,6 @@ time: O(n)
 196418, 317811,
 """
 
-def fourNumberSum(array, targetSum):
-    allPairSums = {}
-    quads = []
 
-    for i in range(1, len(array) - 1):
-        for j in range(i + 1, len(array)):
-            currentSum = array[i] + array[j]
-            difference = targetSum - currentSum
-            if difference in allPairSums:
-                for pair in allPairSums[difference]:
-                    quads.append(pair + [array[i], array[j]])
-
-        for k in range(0, i):
-            currentSum = array[i] + array[k]
-            if currentSum not in allPairSums:
-                allPairSums[currentSum] = [[array[i], array[k]]]
-            else:
-                allPairSums[currentSum].append([array[i], array[k]])
-    return quads
+def fourNumberSum(array):
+    pass
