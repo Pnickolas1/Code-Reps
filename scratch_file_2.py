7365332f3210433d7c31b07e0395e5d3f9eae445
Peter <petertountas1@gmail.com>
2019-09-30 12:28:50 -0500

    added techcrunch csv

--- scratch_file_2.py
+++ scratch_file_2.py
@@ -5,25 +5,6 @@ def swapLeftAndRight(tree):
     tree.left, tree.right = tree.right, tree.left
 
 
-class Node:
-
-    def __init__(self, value):
-        self.value = value
-        self.children = []
-
-    def addChild(self, value):
-        self.children.append(Node(value))
-        return self
-
-    def breadthFirstSearch(self, array):
-        queue = [self]
-        while len(queue) > 0:
-            current = queue.pop(0)
-            array.append(current.value)
-            for child in current.children:
-                queue.append(child)
-        return array
 
 
-    def depthFirstSearch(self, array):
-
+def removeKthNodeFromEnd(head, k):
