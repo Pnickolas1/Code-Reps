3c1c6a87ca74c7856dd5a484f512c8d6319c7863
Peter Tountas <petertountas1@gmail.com>
2019-10-16 22:34:47 -0500

    updated is palindrome nunber

--- EOPI/intersect_rectangle.py
+++ EOPI/intersect_rectangle.py
@@ -28,6 +28,7 @@ r5 = Rectangle(1, 2, 3, 1)
 def intersect_rectangle(R1, R2):
 
     def is_intersect(R1, R2):
+
         return (R1.x <= R2.x + R2.width and R1.x + R1.width >= R2.x and
                 R1.y <= R2.y + R2.height and R1.y + R1.height >= R2.y)
 
--- EOPI/is_palindrome_number.py
+++ EOPI/is_palindrome_number.py
@@ -14,21 +14,6 @@ def is_palindrome_number(x):
     return True
 
 
-def is_palindrome(x):
-    if x < 0:
-        return False
-
-    num_digits = math.floor(math.log10(x)) + 1
-    msd_value = 10**(num_digits - 1)
-    for i in range(num_digits // 2):
-        if x // msd_value != x % 10:
-            return False
-
-        x %= 10
-        x // msd_value
-        msd_value //= 100
-    return True
-
-
 
 print(is_palindrome_number(234432))
+print(is_palindrome_number(153351))
