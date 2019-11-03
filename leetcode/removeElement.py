c2aba65254c5616470c8f50b753e99cc373f4e78
Peter Tountas <petertountas1@gmail.com>
2019-11-03 15:26:38 -0600

    added

---
+++ leetcode/removeElement.py
@@ -0,0 +1,25 @@
+
+"""
+Remove Element
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
+
+"""
+
+def removeElement(arr, val):
+    start = 0
+    last = len(arr) - 1
+    while start <= last:
+        if arr[start] == val:
+            arr[start] = arr[last]
+            arr[last] = arr[start]
+            last -= 1
+        else:
+            start += 1
+    return start
+
+
