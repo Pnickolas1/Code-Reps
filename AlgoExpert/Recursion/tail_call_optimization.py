2ca84c78bb874d3f1a157f71a6953acab49002e8
Peter Tountas <petertountas1@gmail.com>
2020-01-15 19:47:26 -0600

    added

--- AlgoExpert/Recursion/tail_call_optimization.py
+++ AlgoExpert/Recursion/tail_call_optimization.py
@@ -14,6 +14,12 @@ Tail Call optimization:
 - a function call pushes a frame onto the call stack and a return pops a stack off
 
 
+leetcode def:
+Tail recursion is a recursion where the recursive call is the final instruction in the recursion function.
+And there should be only one recursive call in the function.
+
+
+
 """
 
 
@@ -25,3 +31,22 @@ def tail_call_optimizatin(n, accumulator=1):
         return tail_call_optimizatin(n - 1, n * accumulator)
 
 print(tail_call_optimizatin(5))
+
+
+
+
+def sum_non_tail_recursion(ls):
+
+    if len(ls) == 0:
+        return 0
+
+    return ls[0] + sum_non_tail_recursion(ls[1:])
+
+def sum_tail_recursion(ls):
+    def helper(ls, acc):
+
+        if len(ls) == 0:
+            return acc
+
+        return helper(ls[1:], ls[0] + acc)
+    return helper(ls, 0)
