ed5e8506d79e346bd7464d2776d2d0d1111fb1c6
Peter <petertountas1@gmail.com>
2020-02-21 14:14:22 -0600

    added sesrch range

--- scratch_file_1.py
+++ scratch_file_1.py
@@ -15,22 +15,3 @@ sampleInput = [[1,3,4,10],
                [7,13,14,16]]
 wordbank = ['this', 'that', 'apple', 'is', 'apology']
 
-def lowestCommonManager(topManager, reportOne, reportTwo):
-    return getOrgInfo(topManager, reportOne, reportTwo).lowestCommonManager
-
-def getOrgInfo(manager, reportOne, reportTwo):
-    numberImportantReports = 0
-    for directReport in manager.directReports:
-        orgInfo = getOrgInfo(directReport, reportOne, reportTwo)
-        if orgInfo.lowestCommonManager is not None:
-            return orgInfo
-        numberImportantReports += orgInfo.lowestCommonManager
-    if manager == reportOne or manager == reportTwo:
-        numberImportantReports += 1
-    lowestCommonManager = manager if numberImportantReports == 2 else None
-    return OrgInfo(lowestCommonManager, numberImportantReports)
-
-class OrgInfo:
-    def __init__(self, lowestCommonManager, numberImportantReports):
-        self.lowestCommonManager = lowestCommonManager
-        self.numImportantReports = numberImportantReports
